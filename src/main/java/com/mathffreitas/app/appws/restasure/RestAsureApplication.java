131167cc3d2beccf54d7b3c8557b782e65cc6620
Math <mathffreitas@hotmail.com>
2022-09-28 15:14:09 -0300

    Project Structure (with Create User tests)

---
+++ src/main/java/com/mathffreitas/app/appws/restasure/RestAsureApplication.java
@@ -0,0 +1,13 @@
+package com.mathffreitas.app.appws.restasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAsureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mathffreitas/app/appws/restasure/RestAsureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathffreitas.app.appws.restasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAsureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
